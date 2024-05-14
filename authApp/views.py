648953e010073896927f2a7f5c6f72e212e11f6f
dgguzmangr <dgguzmangr@gmail.com>
2024-05-14 01:19:33 -0500

    first project configuration upload

---
+++ authApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class MyAPIView(APIView):
+    def get(self, request):
+        # Código para manejar las solicitudes GET
+        return Response({'message': 'GET request processed'})
+
+    def post(self, request):
+        # Código para manejar las solicitudes POST
+        return Response({'message': 'POST request processed'})
+# Create your views here.
